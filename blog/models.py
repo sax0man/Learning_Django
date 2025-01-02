61349dee12f76840c8f762491eb56632c001a5e6
Tejas Kodoor <109711775+sax0man@users.noreply.github.com>
2025-01-02 08:42:40 +0530

    Added templates

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
-
+    
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
